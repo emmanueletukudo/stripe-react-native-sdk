37d4df840686a21d8c7b005dfa9b39d5a145d5be
emmanueletukudo <emzzyyoung@gmail.com>
2021-05-24 15:47:05 +0100

    update on payment screen

--- screens/paymentScreen.js
+++ screens/paymentScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { StyleSheet, Button, View} from 'react-native';
 import {
   CardField,
   CardFieldInput,
@@ -11,20 +11,19 @@ export default PaymentScreen = () => {
   const [card, setCard] = useState(CardFieldInput.Details | null);
   const { confirmPayment, handleCardAction } = useStripe()
 
-  const API_URL = "http://localhost:8000"
+  const API_URL = "http://localhost:8000";
 
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
   const [loading, setLoading] = useState(false);
 
   const fetchPaymentSheetParams = async () => {
-    const response = await fetch(`${API_URL}/payment-sheet`, {
+    const response = await fetch(`${API_URL}/checkout`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
     });
     const { paymentIntent, ephemeralKey, customer } = await response.json();
-
     return {
       paymentIntent,
       ephemeralKey,
@@ -50,7 +49,13 @@ export default PaymentScreen = () => {
   };
 
   const openPaymentSheet = async () => {
-    // see below
+    const { error } = await presentPaymentSheet({ clientSecret });
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      Alert.alert('Success', 'Your order is confirmed!');
+    }
   };
 
   useEffect(() => {
@@ -58,15 +63,50 @@ export default PaymentScreen = () => {
   }, []);
 
   return (
-
-      <View>
+    <View style={styles.container}>
+      <CardField
+        postalCodeEnabled={false}
+        placeholder={{
+          number: '4242 4242 4242 4242',
+        }}
+        cardStyle={{
+          backgroundColor: '#FFFFFF',
+          textColor: '#000000',
+        }}
+        style={{
+          width: '100%',
+          height: 50,
+          marginVertical: 30,
+        }}
+        onCardChange={(cardDetails) => {
+          setCard(cardDetails);
+        }}
+        onFocus={(focusedField) => {
+          console.log('focusField', focusedField);
+        }}
+      />
         <Button
-          variant="primary"
+          style={styles.button}
           disabled={!loading}
           title="Checkout"
+          color="#841584"
           onPress={openPaymentSheet}
         />
-      </View>
+        </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+     flex: 1,
+     padding: 20,
+     marginHorizontal: 10,
+     marginVertical: 10,
+  },
+  button: {
+     backgroundColor: '#00aeef',
+     borderColor: 'red',
+     borderWidth: 5,
+     borderRadius: 15       
+  }
+})
